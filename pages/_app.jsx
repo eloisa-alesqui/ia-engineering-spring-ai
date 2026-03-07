16a7b86332f795579dee63471a589493712e263b
Eloísa Alés Esquivel <eloisa.alequi@gmail.com>
2026-03-07 01:28:07 +0100

    fix: _app.jsx simplificado para Nextra 3 (layout gestionado internamente)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,5 @@
-import Layout from 'nextra-theme-docs'
 import 'nextra-theme-docs/style.css'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <Layout {...pageProps}>
-      <Component {...pageProps} />
-    </Layout>
-  )
+  return <Component {...pageProps} />
 }
