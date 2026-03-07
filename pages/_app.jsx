4b4d45266ace7e3d150c2f16fd19fefb3ebdd6b0
Eloísa Alés Esquivel <eloisa.alequi@gmail.com>
2026-03-07 11:43:38 +0100

    feat: añadir Google Analytics G-1LQ9TMTP2D

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,25 @@
 import 'nextra-theme-docs/style.css'
 import '../styles/global.css'
+import Script from 'next/script'
+
+const GA_ID = 'G-1LQ9TMTP2D'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
